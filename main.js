c625e60c557fd152c85272f151592a0ded015382
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2015-02-07 16:46:04 +0200

    Add kill others option

--- main.js
+++ main.js
@@ -6,10 +6,20 @@ var program = require('commander');
 var _ = require('lodash');
 var chalk = require('chalk');
 var childProcess = Promise.promisifyAll(require('child_process'));
+require('./lodash-mixins');
 
 var config = {
     // Kill other processes if one dies
-    kill: true
+    killOthers: false,
+
+    // How much in ms we wait before killing other processes
+    killDelay: 1000,
+
+    // Prefix logging with pid
+    prefix: 'pid',
+
+    // By default, color output
+    noColor: false
 };
 
 function main() {
@@ -23,16 +33,16 @@ function parseArgs() {
         .version(require('./package.json').version)
         .usage('[options] [command..]')
         .option(
-            '-n, --no-kill',
-            'prevent killing other processes if one dies'
+            '-k, --kill-others',
+            'kill other processes if one exits or dies'
         );
 
     program.on('--help', function() {
         console.log('  Examples:');
         console.log('');
-        console.log('   - Prevent stopping other processes if one dies');
+        console.log('   - Kill other processes if one exits or dies');
         console.log('');
-        console.log('       $ conc --no-kill "grunt watch" "http-server"');
+        console.log('       $ concurrent --kill-others "grunt watch" "http-server"');
         console.log('');
 
         var url = 'https://github.com/kimmobrunfeldt/node-concurrent';
@@ -44,32 +54,56 @@ function parseArgs() {
 }
 
 function mergeDefaultsWithArgs(config) {
-    return {
-        kill: config.kill
-    };
+    return _.merge(config, program);
 }
 
 function logWithPrefix(prefix, text, color) {
-    var spaceCount = Array(prefix.length + 1).join(' ');
+    var spaceCount = _.repeat(' ', prefix.length);
     var lines = text.split('\n');
 
-    var newText = _.map(lines, function(line, i) {
+    var bold = getColorFunc(chalk.bold);
+
+    var paddedText = _.map(lines, function(line, i) {
         var coloredLine = color ? color(line) : line;
-        if (i > 0) return spaceCount + coloredLine;
-        return chalk.bold(prefix) + coloredLine;
+
+        if (i > 0) {
+            return spaceCount + coloredLine;
+        }
+        return bold(prefix) + coloredLine;
     }).join('\n');
 
-    console.log(newText);
+    console.log(paddedText);
 }
 
-function logError(childProcess, buffer) {
-    var prefix = '[' + childProcess.pid + '] ';
-    logWithPrefix(prefix, buffer.toString(), chalk.red.bold);
+function getColorFunc(chalkAttr) {
+    if (config.noColor) {
+        return function(text) {
+            return text;
+        };
+    } else {
+        return chalkAttr;
+    }
 }
 
-function log(childProcess, buffer) {
-    var prefix = '[' + childProcess.pid + '] ';
+function logError(prefix, buffer) {
+    prefix = '[' + prefix + '] ';
+    logWithPrefix(prefix, buffer.toString(), getColorFunc(chalk.red.bold));
+}
+
+function logEvent(prefix, buffer) {
+    prefix = '[' + prefix + '] ';
+    logWithPrefix(prefix, buffer.toString(), getColorFunc(chalk.gray.dim));
+}
+
+function log(prefix, buffer) {
+    prefix = '[' + prefix + '] ';
     logWithPrefix(prefix, buffer.toString());
+}
+
+// Map data received from child process to
+// format where child information is also embedded
+function mapProcessData(child, data) {
+    return {child: child, data: data};
 }
 
 function run(commands) {
@@ -78,35 +112,76 @@ function run(commands) {
         return childProcess.spawn(_.head(parts), _.tail(parts));
     });
 
-    var outStreams = _.map(children, function(child) {
-        return Rx.Node.fromReadableStream(child.stdout)
-        .map(function(buffer) {
-            return {buffer: buffer, childProcess: child};
+    var aliveChildren = _.clone(children);
+    var exitCodes = [];
+
+    // Transform all process events to rx streams
+    var streams = _.map(children, function(child) {
+        var streamList = [
+            Rx.Node.fromReadableStream(child.stdout),
+            Rx.Node.fromReadableStream(child.stderr),
+            Rx.Node.fromEvent(child, 'error'),
+            Rx.Node.fromEvent(child, 'exit')
+        ];
+
+        var mappedStreams = _.map(streamList, function(stream) {
+            return stream.map(function(data) {
+                return {child: child, data: data};
+            });
         });
+
+        return {
+            stdout: mappedStreams[0],
+            stderr: mappedStreams[1],
+            error: mappedStreams[2],
+            exit: mappedStreams[3]
+        };
     });
 
-    var errStreams = _.map(children, function(child) {
-        return Rx.Node.fromReadableStream(child.stderr)
-        .map(function(buffer) {
-            return {buffer: buffer, childProcess: child};
+    var stdoutStreams = _.pluck(streams, 'stdout');
+    var stdoutStream = Rx.Observable.merge.apply(this, stdoutStreams);
+
+    var stderrStreams = _.pluck(streams, 'stderr');
+    var stderrStream = Rx.Observable.merge.apply(this, stderrStreams);
+
+    stdoutStream.subscribe(function(event) {
+        var prefix = event.child.pid;
+        log(prefix, event.data);
+    });
+
+    stderrStream.subscribe(function(event) {
+        var prefix = event.child.pid;
+        logError(prefix, event.data);
+    });
+
+    var exitStreams = _.pluck(streams, 'exit');
+    var exitStream = Rx.Observable.merge.apply(this, exitStreams);
+
+    exitStream.subscribe(function(event) {
+        var exitCode = event.data;
+        logEvent(event.child.pid, 'Exit with code ' + exitCode);
+        exitCodes.push(exitCode);
+
+        aliveChildren = _.filter(aliveChildren, function(child) {
+            return child.pid !== event.child.pid;
         });
+
+        if (aliveChildren.length === 0) {
+            process.exit(_.sum(exitCodes));
+        }
     });
 
-    var mergedOut = _.reduce(_.tail(outStreams), function(memo, stream) {
-        return memo.merge(stream);
-    }, _.head(outStreams));
+    if (config.killOthers) {
+        var delayedExit = exitStream.delay(config.killDelay);
+        delayedExit.subscribe(function() {
+            logEvent('main', 'Sending SIGTERM to other processes..');
 
-    var mergedErr = _.reduce(_.tail(errStreams), function(memo, stream) {
-        return memo.merge(stream);
-    }, _.head(errStreams));
-
-    mergedOut.subscribe(function(data) {
-        log(data.childProcess, data.buffer);
-    });
-
-    mergedErr.subscribe(function(data) {
-        logError(data.childProcess, data.buffer);
-    });
+            // Send SIGTERM to alive children
+            _.each(aliveChildren, function(child) {
+                child.kill();
+            });
+        });
+    }
 }
 
 main();
