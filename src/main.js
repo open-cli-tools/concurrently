2f6019ba0f3633f9a68435a2254c1a7453087f9b
Marcio Junior <marciojunior_eu@yahoo.com.br>
2016-07-12 11:02:56 -0300

    Send SIGTERM signal instead SIGINT

--- src/main.js
+++ src/main.js
@@ -313,7 +313,7 @@ function handleClose(streams, children, childrenInfo) {
                 if (isWindows) {
                     spawn('taskkill', ["/pid", child.pid, '/f', '/t']);
                 } else {
-                    child.kill('SIGINT');
+                    child.kill('SIGTERM');
                 }
             });
         });
