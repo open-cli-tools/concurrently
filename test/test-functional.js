d49e04d57c1f14ef245368b0db52acae0d5c744b
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2016-09-20 00:19:28 +0300

    Change to use promises with mocha

--- test/test-functional.js
+++ test/test-functional.js
@@ -15,75 +15,67 @@ process.chdir(path.join(testDir, '..'));
 describe('concurrently', function() {
     this.timeout(5000);
 
-    it('help should be successful', function(done) {
-        run('node ./src/main.js --help', {pipe: DEBUG_TESTS})
-        .then(function(exitCode) {
-            // exit code 0 means success
-            assert.strictEqual(exitCode, 0);
-            done();
-        });
+    it('help should be successful', () => {
+        return run('node ./src/main.js --help', {pipe: DEBUG_TESTS})
+            .then(function(exitCode) {
+                // exit code 0 means success
+                assert.strictEqual(exitCode, 0);
+            });
     });
 
-    it('version should be successful', function(done) {
-        run('node ./src/main.js -V', {pipe: DEBUG_TESTS})
-        .then(function(exitCode) {
-            assert.strictEqual(exitCode, 0);
-            done();
-        });
+    it('version should be successful', () => {
+        return run('node ./src/main.js -V', {pipe: DEBUG_TESTS})
+            .then(function(exitCode) {
+                assert.strictEqual(exitCode, 0);
+            });
     });
 
-    it('two successful commands should exit 0', function(done) {
-        run('node ./src/main.js "echo test" "echo test"', {pipe: DEBUG_TESTS})
-        .then(function(exitCode) {
-            assert.strictEqual(exitCode, 0);
-            done();
-        });
+    it('two successful commands should exit 0', () => {
+        return run('node ./src/main.js "echo test" "echo test"', {pipe: DEBUG_TESTS})
+            .then(function(exitCode) {
+                assert.strictEqual(exitCode, 0);
+            });
     });
 
-    it('at least one unsuccessful commands should exit non-zero', function(done) {
-        run('node ./src/main.js "echo test" "return 1" "echo test"', {pipe: DEBUG_TESTS})
-        .then(function(exitCode) {
-            assert.notStrictEqual(exitCode, 0);
-            done();
-        });
+    it('at least one unsuccessful commands should exit non-zero', () => {
+        return run('node ./src/main.js "echo test" "return 1" "echo test"', {pipe: DEBUG_TESTS})
+            .then(function(exitCode) {
+                assert.notStrictEqual(exitCode, 0);
+            });
     });
 
-    it('--kill-others should kill other commands if one dies', function(done) {
+    it('--kill-others should kill other commands if one dies', () => {
         // This test would timeout if kill others option does not work
-        run('node ./src/main.js --kill-others "sleep 1000" "echo test" "sleep 1000"', {pipe: DEBUG_TESTS})
-        .then(function(exitCode) {
-            assert.notStrictEqual(exitCode, 0);
-            done();
-        });
+        return run('node ./src/main.js --kill-others "sleep 1000" "echo test" "sleep 1000"', {pipe: DEBUG_TESTS})
+            .then(function(exitCode) {
+                assert.notStrictEqual(exitCode, 0);
+            });
     });
 
-    it('--success=first should return first exit code', function(done) {
-        run('node ./src/main.js -k --success first "echo test" "sleep 1000" ', {pipe: DEBUG_TESTS})
+    it('--success=first should return first exit code', () => {
+        return run('node ./src/main.js -k --success first "echo test" "sleep 1000" ', {pipe: DEBUG_TESTS})
+            // When killed, sleep returns null exit code
+            .then(function(exitCode) {
+                assert.strictEqual(exitCode, 0);
+            });
+    });
+
+    it('--success=last should return last exit code', () => {
         // When killed, sleep returns null exit code
-        .then(function(exitCode) {
-            assert.strictEqual(exitCode, 0);
-            done();
-        });
+        return run('node ./src/main.js -k --success last "echo test" "sleep 1000" ', {pipe: DEBUG_TESTS})
+            .then(function(exitCode) {
+                assert.notStrictEqual(exitCode, 0);
+            });
     });
 
-    it('--success=last should return last exit code', function(done) {
-        // When killed, sleep returns null exit code
-        run('node ./src/main.js -k --success last "echo test" "sleep 1000" ', {pipe: DEBUG_TESTS})
-        .then(function(exitCode) {
-            assert.notStrictEqual(exitCode, 0);
-            done();
-        });
+    it('&& return 1 should return non-zero exit code', () => {
+        return run('node ./src/main.js "echo 1 && return 1" "echo 2 && return 2" ', {pipe: DEBUG_TESTS})
+            .then(function(exitCode) {
+                assert.strictEqual(exitCode, 1);
+            });
     });
 
-    it('&& return 1 should return non-zero exit code', function(done) {
-        run('node ./src/main.js "echo 1 && return 1" "echo 2 && return 2" ', {pipe: DEBUG_TESTS})
-        .then(function(exitCode) {
-            assert.strictEqual(exitCode, 1);
-            done();
-        });
-    });
-
-    ['SIGINT', 'SIGTERM'].forEach(function(signal) {
+    ['SIGINT', 'SIGTERM'].forEach((signal) => {
       it('killing it with ' + signal + ' should propagate the signal to the children', function(done) {
         var readline = require('readline');
         var waitingStart = 2;
