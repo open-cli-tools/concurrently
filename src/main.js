open-cli-tools/concurrently

commit 16260ff8d6f7dc49b78e18433b95d9b2f7728970

Fix stdout when lines are cleared (#70)
Some ANSI code where not well processed like those that clear the entire line or move the cursor around.
Still need to support moving the cursor to far left as its conflicting with the coloredPrefix.
https://en.wikipedia.org/wiki/ANSI_escape_code

src/main.js
@@ -478,31 +478,35 @@ function logError(prefix, prefixColor, text) {
     logWithPrefix(prefix, prefixColor, text, chalk.red.bold);
 }
 
-function logWithPrefix(prefix, prefixColor, text, color) {
-    var lastChar = text[text.length - 1];
-    if (config.raw) {
-        if (lastChar !== '\n') {
-            text += '\n';
-        }
+var lastChar;
 
+function logWithPrefix(prefix, prefixColor, text, color) {
+
+     if (config.raw) {  
         process.stdout.write(text);
         return;
     }
 
-    if (lastChar === '\n') {
-        // Remove extra newline from the end to prevent extra newlines in input
-        text = text.slice(0, text.length - 1);
-    }
+    text = text.replace(/\u2026/g,'...'); // Ellipsis
 
     var lines = text.split('\n');
     // Do not bgColor trailing space
     var coloredPrefix = colorText(prefix.replace(/ $/, ''), prefixColor) + ' ';
-    var paddedLines = _.map(lines, function(line, i) {
+    var paddedLines = _.map(lines, function(line, index) {
         var coloredLine = color ? colorText(line, color) : line;
-        return coloredPrefix + coloredLine;
+        if (index !== 0 && index !== (lines.length - 1)) {
+            coloredLine = coloredPrefix + coloredLine;
+        }
+        return coloredLine;
     });
 
-    console.log(paddedLines.join('\n'));
+    if (!lastChar || lastChar == '\n' ){
+        process.stdout.write(coloredPrefix);
+    }
+
+    lastChar = text[text.length - 1];         
+
+    process.stdout.write(paddedLines.join('\n'));    
 }
 
 main();

commit bd3b6021c62d34d5890abe427dc3358af66cabc3

Log event messages with a newline in the end
This broke after #70 was merged.

src/main.js
@@ -469,7 +469,7 @@ function log(prefix, prefixColor, text) {
 function logEvent(prefix, prefixColor, text) {
     if (config.raw) return;
 
-    logWithPrefix(prefix, prefixColor, text, chalk.gray.dim);
+    logWithPrefix(prefix, prefixColor, text + '\n', chalk.gray.dim);
 }
 
 function logError(prefix, prefixColor, text) {
@@ -482,7 +482,7 @@ var lastChar;
 
 function logWithPrefix(prefix, prefixColor, text, color) {
 
-     if (config.raw) {  
+     if (config.raw) {
         process.stdout.write(text);
         return;
     }
@@ -504,9 +504,9 @@ function logWithPrefix(prefix, prefixColor, text, color) {
         process.stdout.write(coloredPrefix);
     }
 
-    lastChar = text[text.length - 1];         
+    lastChar = text[text.length - 1];
 
-    process.stdout.write(paddedLines.join('\n'));    
+    process.stdout.write(paddedLines.join('\n'));
 }
 
 main();