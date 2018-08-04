3f0e88b8f764acef8f993a6bbc1feda1a663ce46
Gustavo Henke <guhenke@gmail.com>
2018-08-04 14:02:06 +1000

    Test InputHandler

--- src/flow-control/input-handler.js
+++ src/flow-control/input-handler.js
@@ -2,35 +2,37 @@ const Rx = require('rxjs');
 const { map } = require('rxjs/operators');
 
 module.exports = class InputHandler {
-    constructor({ defaultInputTarget, inputStream, logger }) {
+    constructor({ defaultInputTarget, inputStream, logger, scheduler }) {
         this.defaultInputTarget = defaultInputTarget;
         this.inputStream = inputStream;
         this.logger = logger;
+        this.scheduler = scheduler;
     }
 
     handle(commands) {
         if (!this.inputStream) {
-            return Rx.of(null);
+            return Rx.of(null, this.scheduler);
         }
 
         Rx.fromEvent(this.inputStream, 'data')
             .pipe(map(data => data.toString()))
             .subscribe(data => {
-                let [targetId, input] = data.split(':', 2); // eslint-disable-line prefer-const
+                let [targetId, input] = data.split(':', 2);
                 targetId = input ? targetId : this.defaultInputTarget;
+                input = input || data;
 
-                const command = commands.filter(command => (
+                const command = commands.find(command => (
                     command.name === targetId ||
-                    command.index.toString() === targetId
+                    command.index.toString() === targetId.toString()
                 ));
 
                 if (command && command.stdin) {
-                    command.stdin.write(line);
+                    command.stdin.write(input);
                 } else {
-                    this.logger.logGlobalEvent(`Unable to find command ${targetId}\n`);
+                    this.logger.logGlobalEvent(`Unable to find command ${targetId}, or it has no stdin open\n`);
                 }
             });
 
-        return Rx.of(null);
+        return Rx.of(null, this.scheduler);
     }
 };
