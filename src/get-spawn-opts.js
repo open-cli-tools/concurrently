7e78e98916b2852a6acb12d96f975f30d4738936
Gustavo Henke <guhenke@gmail.com>
2018-08-05 01:11:19 +1000

    Test getSpawnOpts

--- src/get-spawn-opts.js
+++ src/get-spawn-opts.js
@@ -2,9 +2,9 @@ const supportsColor = require('supports-color');
 
 module.exports = ({
     colorSupport = supportsColor,
-    isWindows = process.platform
+    process = global.process
 } = {}) => Object.assign(
     {},
-    isWindows && { detached: false },
-    colorSupport && Object.assign({ FORCE_COLOR: colorSupport.level }, process.env)
+    /^win/.test(process.platform) && { detached: false },
+    colorSupport && { env: Object.assign({ FORCE_COLOR: colorSupport.level }, process.env) }
 );
---
+++ src/get-spawn-opts.spec.js
@@ -0,0 +1,14 @@
+const getSpawnOpts = require('./get-spawn-opts');
+
+it('sets detached mode to false for Windows platform', () => {
+    expect(getSpawnOpts({ process: { platform: 'win32' } }).detached).toBe(false);
+});
+
+it('merges FORCE_COLOR into env vars if color supported', () => {
+    const process = { env: { foo: 'bar' } };
+    expect(getSpawnOpts({ process, colorSupport: false }).env).toBeUndefined();
+    expect(getSpawnOpts({ process, colorSupport: { level: 1 } }).env).toEqual({
+        FORCE_COLOR: 1,
+        foo: 'bar'
+    });
+});
