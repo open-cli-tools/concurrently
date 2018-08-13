05ca033fa99da77c90058d7a8ad9a0f386574e2b
Gustavo Henke <guhenke@gmail.com>
2018-08-14 01:25:11 +1000

    Add tests for binary

---
+++ bin/fixtures/read-echo.js
@@ -0,0 +1,10 @@
+process.stdin.on('data', (chunk) => {
+    const line = chunk.toString().trim();
+    console.log(line);
+
+    if (line === 'stop') {
+        process.exit(0);
+    }
+});
+
+console.log('READING');
