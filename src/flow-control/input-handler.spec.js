130de71134113ad1323c0d5aaa3d11503f986228
Brandon Chinn <brandonchinn178@gmail.com>
2020-12-25 15:06:50 -0800

    Use actual Readable in input-handler tests

--- src/flow-control/input-handler.spec.js
+++ src/flow-control/input-handler.spec.js
@@ -1,4 +1,4 @@
-const EventEmitter = require('events');
+const stream = require('stream');
 const { createMockInstance } = require('jest-create-mock-instance');
 
 const Logger = require('../logger');
@@ -12,7 +12,7 @@ beforeEach(() => {
         createFakeCommand('foo', 'echo foo', 0),
         createFakeCommand('bar', 'echo bar', 1),
     ];
-    inputStream = new EventEmitter();
+    inputStream = new stream.PassThrough();
     logger = createMockInstance(Logger);
     controller = new InputHandler({
         defaultInputTarget: 0,
@@ -31,7 +31,7 @@ it('returns same commands', () => {
 it('forwards input stream to default target ID', () => {
     controller.handle(commands);
 
-    inputStream.emit('data', Buffer.from('something'));
+    inputStream.write('something');
 
     expect(commands[0].stdin.write).toHaveBeenCalledTimes(1);
     expect(commands[0].stdin.write).toHaveBeenCalledWith('something');
@@ -41,7 +41,7 @@ it('forwards input stream to default target ID', () => {
 it('forwards input stream to target index specified in input', () => {
     controller.handle(commands);
 
-    inputStream.emit('data', Buffer.from('1:something'));
+    inputStream.write('1:something');
 
     expect(commands[0].stdin.write).not.toHaveBeenCalled();
     expect(commands[1].stdin.write).toHaveBeenCalledTimes(1);
@@ -63,7 +63,7 @@ it('forwards input stream to target index specified in input when input contains
 it('forwards input stream to target name specified in input', () => {
     controller.handle(commands);
 
-    inputStream.emit('data', Buffer.from('bar:something'));
+    inputStream.write('bar:something');
 
     expect(commands[0].stdin.write).not.toHaveBeenCalled();
     expect(commands[1].stdin.write).toHaveBeenCalledTimes(1);
@@ -74,7 +74,7 @@ it('logs error if command has no stdin open', () => {
     commands[0].stdin = null;
     controller.handle(commands);
 
-    inputStream.emit('data', Buffer.from('something'));
+    inputStream.write('something');
 
     expect(commands[1].stdin.write).not.toHaveBeenCalled();
     expect(logger.logGlobalEvent).toHaveBeenCalledWith('Unable to find command 0, or it has no stdin open\n');
@@ -83,7 +83,7 @@ it('logs error if command has no stdin open', () => {
 it('logs error if command is not found', () => {
     controller.handle(commands);
 
-    inputStream.emit('data', Buffer.from('foobar:something'));
+    inputStream.write('foobar:something');
 
     expect(commands[0].stdin.write).not.toHaveBeenCalled();
     expect(commands[1].stdin.write).not.toHaveBeenCalled();
