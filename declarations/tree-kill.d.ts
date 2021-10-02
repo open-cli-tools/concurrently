3293ccd89b0c5ebbed23cc22e7a37eb24f50c53d
Gustavo Henke <guhenke@gmail.com>
2021-10-02 20:18:49 +1000

    Improve tree-kill declaration

--- declarations/tree-kill.d.ts
+++ declarations/tree-kill.d.ts
@@ -1,4 +1,4 @@
 declare module 'tree-kill' {
-    function treeKill(pid: number, signal?: string): void;
+    function treeKill(pid: number, signal?: NodeJS.Signals): void;
     export = treeKill;
 }
