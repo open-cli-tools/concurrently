658e6be340748f4c703d0b235d00b8c67354446e
Pascal Jufer <pascal-jufer@bluewin.ch>
2022-05-02 10:46:24 +0200

    More specific placeholder regex & reformat file

--- src/command-parser/expand-arguments.ts
+++ src/command-parser/expand-arguments.ts
@@ -6,29 +6,39 @@ import { quote } from 'shell-quote';
  * Replace placeholders with additional arguments.
  */
 export class ExpandArguments implements CommandParser {
-    constructor(private readonly additionalArguments: string[]) {
-    }
+    constructor(private readonly additionalArguments: string[]) {}
 
     parse(commandInfo: CommandInfo) {
-        const command = commandInfo.command.replace(/\\?{([@\*]|\d+)\}/g, (match, placeholderTarget) => {
-            // Don't replace the placeholder if it is escaped by a backslash.
-            if (match.startsWith('\\')) {
-                return match.substring(1);
-            }
-            if (!isNaN(placeholderTarget) && placeholderTarget > 0 && placeholderTarget <= this.additionalArguments.length) {
-                return quote([this.additionalArguments[placeholderTarget-1]]);
-            }
-            if (placeholderTarget === '@') {
-                return quote(this.additionalArguments);
-            }
-            if (placeholderTarget === '*') {
-                return quote([this.additionalArguments.join(' ')]);
-            }
-            return '';
-        });
+        const command = commandInfo.command.replace(
+            /\\?\{([@\*]|[1-9][0-9]*)\}/g,
+            (match, placeholderTarget) => {
+                // Don't replace the placeholder if it is escaped by a backslash.
+                if (match.startsWith('\\')) {
+                    return match.substring(1);
+                }
+                // Replace numeric placeholder if value exists in additional arguments.
+                if (
+                    !isNaN(placeholderTarget) &&
+                    placeholderTarget <= this.additionalArguments.length
+                ) {
+                    return quote([this.additionalArguments[placeholderTarget - 1]]);
+                }
+                // Replace all arguments placeholder.
+                if (placeholderTarget === '@') {
+                    return quote(this.additionalArguments);
+                }
+                // Replace combined arguments placeholder.
+                if (placeholderTarget === '*') {
+                    return quote([this.additionalArguments.join(' ')]);
+                }
+                // Replace placeholder with empty string
+                // if value doesn't exist in additional arguments.
+                return '';
+            },
+        );
 
         return Object.assign({}, commandInfo, {
             command,
         });
     }
-};
+}
