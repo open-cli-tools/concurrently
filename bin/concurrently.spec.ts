dc2b59b4eb8d6ae899500326036c222bf03e0c7e
Pascal Jufer <pascal-jufer@bluewin.ch>
2022-08-09 13:29:54 +0200

    Revert "Switch from 'swc' to 'esr' for cli tests"
    This reverts commit 22e2a96a5da31b9a102a4ccc5c1925c42ed1c518.

--- bin/concurrently.spec.ts
+++ bin/concurrently.spec.ts
@@ -15,8 +15,9 @@ const createKillMessage = (prefix: string) =>
  * Returns observables for its combined stdout + stderr output, close events, pid, and stdin stream.
  */
 const run = (args: string) => {
-    // Using '--cache' means the first run is slower, but subsequent runs are then much faster.
-    const child = spawn(`esr --cache ./concurrently.ts ${args}`, {
+    // TODO: Optimally, this should only be transpiled once,
+    //       e.g. bundle in `beforeAll` and then reuse here.
+    const child = spawn(`node -r @swc-node/register ./concurrently.ts ${args}`, {
         shell: true,
         cwd: __dirname,
         env: {
