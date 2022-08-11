42eeaa896f9b6609c00043457f6199bf54f2f952
Pascal Jufer <pascal-jufer@bluewin.ch>
2022-08-11 10:55:28 +0200

    Refactor test method in src/command.spec.ts

--- src/command.spec.ts
+++ src/command.spec.ts
@@ -2,10 +2,13 @@ import { SpawnOptions } from 'child_process';
 import { EventEmitter } from 'events';
 import { Readable, Writable } from 'stream';
 import { ChildProcess, Command, CommandInfo, KillProcess, SpawnCommand } from './command';
+import { subscribeSpyTo, autoUnsubscribe } from '@hirez_io/observer-spy';
 
 let process: ChildProcess;
 let spawn: jest.Mocked<SpawnCommand>;
 let killProcess: KillProcess;
+
+autoUnsubscribe();
 
 beforeEach(() => {
     process = new (class extends EventEmitter {
@@ -49,139 +52,94 @@ describe('#start()', () => {
 
     it('sets stdin, process and PID', () => {
         const command = createCommand();
-
         command.start();
+
         expect(command.process).toBe(process);
         expect(command.pid).toBe(process.pid);
         expect(command.stdin).toBe(process.stdin);
     });
 
-    it('shares errors to the error stream', done => {
+    it('shares errors to the error stream', () => {
         const command = createCommand();
-        command.error.subscribe(data => {
-            expect(data).toBe('foo');
-            expect(command.process).toBeUndefined();
-            done();
-        });
-
+        const observerSpy = subscribeSpyTo(command.error);
         command.start();
         process.emit('error', 'foo');
+
+        expect(observerSpy.getFirstValue()).toBe('foo');
+        expect(command.process).toBeUndefined();
     });
 
-    it('shares start and close timing events to the timing stream', done => {
+    it('shares start and close timing events to the timing stream', () => {
         const command = createCommand();
-
+        const observerSpy = subscribeSpyTo(command.timer);
         const startDate = new Date();
         const endDate = new Date(startDate.getTime() + 1000);
         jest.spyOn(Date, 'now')
             .mockReturnValueOnce(startDate.getTime())
             .mockReturnValueOnce(endDate.getTime());
-
-        let callCount = 0;
-        command.timer.subscribe(({ startDate: actualStartDate, endDate: actualEndDate }) => {
-            switch (callCount) {
-                case 0:
-                    expect(actualStartDate).toStrictEqual(startDate);
-                    expect(actualEndDate).toBeUndefined();
-                    break;
-                case 1:
-                    expect(actualStartDate).toStrictEqual(startDate);
-                    expect(actualEndDate).toStrictEqual(endDate);
-                    done();
-                    break;
-                default:
-                    throw new Error('Unexpected call count');
-            }
-            callCount++;
-        });
-
         command.start();
         process.emit('close', 0, null);
+
+        expect(observerSpy.getValueAt(0)).toEqual({ startDate, endDate: undefined });
+        expect(observerSpy.getValueAt(1)).toEqual({ startDate, endDate });
     });
 
-    it('shares start and error timing events to the timing stream', done => {
+    it('shares start and error timing events to the timing stream', () => {
         const command = createCommand();
-
+        const observerSpy = subscribeSpyTo(command.timer);
         const startDate = new Date();
         const endDate = new Date(startDate.getTime() + 1000);
         jest.spyOn(Date, 'now')
             .mockReturnValueOnce(startDate.getTime())
             .mockReturnValueOnce(endDate.getTime());
-
-        let callCount = 0;
-        command.timer.subscribe(({ startDate: actualStartDate, endDate: actualEndDate }) => {
-            switch (callCount) {
-                case 0:
-                    expect(actualStartDate).toStrictEqual(startDate);
-                    expect(actualEndDate).toBeUndefined();
-                    break;
-                case 1:
-                    expect(actualStartDate).toStrictEqual(startDate);
-                    expect(actualEndDate).toStrictEqual(endDate);
-                    done();
-                    break;
-                default:
-                    throw new Error('Unexpected call count');
-            }
-            callCount++;
-        });
-
         command.start();
         process.emit('error', 0, null);
+
+        expect(observerSpy.getValueAt(0)).toEqual({ startDate, endDate: undefined });
+        expect(observerSpy.getValueAt(1)).toEqual({ startDate, endDate });
     });
 
-    it('shares closes to the close stream with exit code', done => {
+    it('shares closes to the close stream with exit code', () => {
         const command = createCommand();
-
-        command.close.subscribe(data => {
-            expect(data.exitCode).toBe(0);
-            expect(data.killed).toBe(false);
-            expect(command.process).toBeUndefined();
-            done();
-        });
-
+        const observerSpy = subscribeSpyTo(command.close);
         command.start();
         process.emit('close', 0, null);
+
+        expect(observerSpy.getFirstValue()).toMatchObject({ exitCode: 0, killed: false });
+        expect(command.process).toBeUndefined();
     });
 
-    it('shares closes to the close stream with signal', done => {
+    it('shares closes to the close stream with signal', () => {
         const command = createCommand();
-
-        command.close.subscribe(data => {
-            expect(data.exitCode).toBe('SIGKILL');
-            expect(data.killed).toBe(false);
-            done();
-        });
-
+        const observerSpy = subscribeSpyTo(command.close);
         command.start();
         process.emit('close', null, 'SIGKILL');
+
+        expect(observerSpy.getFirstValue()).toMatchObject({ exitCode: 'SIGKILL', killed: false });
     });
 
-    it('shares closes to the close stream with timing information', done => {
+    it('shares closes to the close stream with timing information', () => {
         const command = createCommand();
-
+        const observerSpy = subscribeSpyTo(command.close);
         const startDate = new Date();
         const endDate = new Date(startDate.getTime() + 1000);
         jest.spyOn(Date, 'now')
             .mockReturnValueOnce(startDate.getTime())
             .mockReturnValueOnce(endDate.getTime());
-
         jest.spyOn(global.process, 'hrtime')
             .mockReturnValueOnce([0, 0])
             .mockReturnValueOnce([1, 1e8]);
-
-        command.close.subscribe(data => {
-            expect(data.timings.startDate).toStrictEqual(startDate);
-            expect(data.timings.endDate).toStrictEqual(endDate);
-            expect(data.timings.durationSeconds).toBe(1.1);
-            done();
-        });
-
         command.start();
         process.emit('close', null, 'SIGKILL');
+
+        expect(observerSpy.getFirstValue().timings).toStrictEqual({
+            startDate,
+            endDate,
+            durationSeconds: 1.1,
+        });
     });
 
-    it('shares closes to the close stream with command info', done => {
+    it('shares closes to the close stream with command info', () => {
         const commandInfo = {
             command: 'cmd',
             name: 'name',
@@ -189,39 +147,30 @@ describe('#start()', () => {
             env: { VAR: 'yes' },
         };
         const command = createCommand(commandInfo);
-
-        command.close.subscribe(data => {
-            expect(data.command).toEqual(expect.objectContaining(commandInfo));
-            expect(data.killed).toBe(false);
-            done();
-        });
-
+        const observerSpy = subscribeSpyTo(command.close);
         command.start();
         process.emit('close', 0, null);
+
+        expect(observerSpy.getFirstValue().command).toEqual(expect.objectContaining(commandInfo));
+        expect(observerSpy.getFirstValue().killed).toBe(false);
     });
 
-    it('shares stdout to the stdout stream', done => {
+    it('shares stdout to the stdout stream', () => {
         const command = createCommand();
-
-        command.stdout.subscribe(data => {
-            expect(data.toString()).toBe('hello');
-            done();
-        });
-
+        const observerSpy = subscribeSpyTo(command.stdout);
         command.start();
         process.stdout.emit('data', Buffer.from('hello'));
+
+        expect(observerSpy.getFirstValue().toString()).toBe('hello');
     });
 
-    it('shares stderr to the stdout stream', done => {
+    it('shares stderr to the stdout stream', () => {
         const command = createCommand();
-
-        command.stderr.subscribe(data => {
-            expect(data.toString()).toBe('dang');
-            done();
-        });
-
+        const observerSpy = subscribeSpyTo(command.stderr);
         command.start();
         process.stderr.emit('data', Buffer.from('dang'));
+
+        expect(observerSpy.getFirstValue().toString()).toBe('dang');
     });
 });
 
@@ -255,16 +204,12 @@ describe('#kill()', () => {
         expect(killProcess).not.toHaveBeenCalled();
     });
 
-    it('marks the command as killed', done => {
+    it('marks the command as killed', () => {
         command.start();
-
-        command.close.subscribe(data => {
-            expect(data.exitCode).toBe(1);
-            expect(data.killed).toBe(true);
-            done();
-        });
-
+        const observerSpy = subscribeSpyTo(command.close);
         command.kill();
         process.emit('close', 1, null);
+
+        expect(observerSpy.getFirstValue()).toMatchObject({ exitCode: 1, killed: true });
     });
 });
