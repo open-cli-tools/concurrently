40d017341367a8cc46e8cb654e040d8f49c67534
Pascal Jufer <pascal-jufer@bluewin.ch>
2022-11-14 00:30:19 +0100

    Report SIGINT

--- bin/fixtures/read-echo.js
+++ bin/fixtures/read-echo.js
@@ -1,5 +1,10 @@
 /* eslint-disable no-console */
 
+process.on('SIGINT', () => {
+    console.log('Received SIGINT');
+    process.exit(0);
+});
+
 process.stdin.on('data', (chunk) => {
     const line = chunk.toString().trim();
     console.log(line);
