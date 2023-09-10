f4fdd59897991ba03ade66496ed7d84091a371a5
Alex Miller <codex.nz@gmail.com>
2023-09-11 11:01:24 +1200

    Fix: concurrently esm default export undefined

--- index.mjs
+++ index.mjs
@@ -7,4 +7,6 @@ import concurrently from './dist/src/index.js';
 // NOTE: the star reexport doesn't work in Node <12.20, <14.13 and <15.
 export * from './dist/src/index.js';
 
-export default concurrently.default;
+// In some cases, like with Bun, `default` is undefined and the function itself is
+// safely accessible via `concurrently`.
+export default concurrently.default || concurrently;
