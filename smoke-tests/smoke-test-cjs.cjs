ee853011adf4d5c2cc6f6c8861042c0407f73ac9
Alex Miller <codex.nz@gmail.com>
2023-09-19 13:21:21 +1200

    Test: Unnecessary async on basic tests

--- smoke-tests/smoke-test-cjs.cjs
+++ smoke-tests/smoke-test-cjs.cjs
@@ -4,21 +4,11 @@
 const assert = require('node:assert');
 const concurrently = require('../index.js');
 
-(async () => {
-  try {
-    // Assert the functions loaded by checking their names load and types are correct
-    assert.strictEqual(
-      typeof concurrently === 'function',
-      true,
-      'Expected default to be function',
-    );
+// Assert the functions loaded by checking their names load and types are correct
+assert.strictEqual(
+  typeof concurrently === 'function',
+  true,
+  'Expected default to be function',
+);
 
-    console.info('Imported cjs successfully');
-  } catch (error) {
-    console.error(error);
-    console.debug(error.stack);
-
-    // Prevent an unhandled rejection, exit gracefully.
-    process.exit(1);
-  }
-})();
+console.info('Imported cjs successfully');
