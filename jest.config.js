cdbdb522ac82adbf7fb5eb9941b5e5a8fffc5c1d
Gustavo Henke <guhenke@gmail.com>
2024-01-05 16:18:16 -0300

    Split test config into separate projects

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,29 @@
+// Need to extend from a base config because projects don't inherit configurations as documented
+// https://github.com/jestjs/jest/issues/11411
+/** @type {import('@jest/types').Config.InitialProjectOptions} */
+const baseConfig = {
+    transform: {
+        '.*': ['@swc/jest'],
+    },
+    testPathIgnorePatterns: ['/node_modules/', '/dist'],
+};
+
 /** @type {import('@jest/types').Config.InitialOptions} */
 const config = {
-    transform: {
-        '^.+\\.(t|j)sx?$': ['@swc/jest'],
-    },
     collectCoverage: true,
     collectCoverageFrom: ['src/**/*.ts', '!src/index.ts'],
-    testPathIgnorePatterns: ['/node_modules/', '/dist'],
+    projects: [
+        {
+            ...baseConfig,
+            displayName: 'unit',
+            testMatch: ['<rootDir>/(src|bin)/**/*.spec.ts'],
+        },
+        {
+            ...baseConfig,
+            displayName: 'smoke',
+            testMatch: ['<rootDir>/tests/**/*.spec.ts'],
+        },
+    ],
 };
 
 module.exports = config;
