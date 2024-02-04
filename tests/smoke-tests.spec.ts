ae6dc869590951a937afca583179a747af666b8c
Gustavo Henke <guhenke@gmail.com>
2024-02-04 20:47:27 +1100

    Increase test timeout

--- tests/smoke-tests.spec.ts
+++ tests/smoke-tests.spec.ts
@@ -8,8 +8,14 @@ beforeAll(async () => {
     await exec('pnpm install', { cwd: __dirname });
 }, 10000);
 
-it.each(['cjs-import', 'cjs-require', 'esm'])('%s', async (project) => {
-    // Use as separate execs as tsc outputs to stdout, instead of stderr, and so its text isn't shown
-    await exec(`tsc -p ${project}`, { cwd: __dirname }).catch((err) => Promise.reject(err.stdout));
-    await exec(`node ${project}/dist/smoke-test.js`, { cwd: __dirname });
-});
+it.each(['cjs-import', 'cjs-require', 'esm'])(
+    '%s',
+    async (project) => {
+        // Use as separate execs as tsc outputs to stdout, instead of stderr, and so its text isn't shown
+        await exec(`tsc -p ${project}`, { cwd: __dirname }).catch((err) =>
+            Promise.reject(err.stdout),
+        );
+        await exec(`node ${project}/dist/smoke-test.js`, { cwd: __dirname });
+    },
+    10000,
+);
