89bcb3ddf72295c843e364797a1917141012a176
Gustavo Henke <guhenke@gmail.com>
2025-07-06 22:54:57 +1000

    type: "module"

--- index.d.ts
+++ index.d.ts
@@ -4,8 +4,4 @@
 import { concurrently } from './dist/src/index.js';
 
 export * from './dist/src/index.js';
-// @ts-expect-error ignore the usage of `export =` along with `export default`.
-// This is seemingly fine, but the file needs to be included in the TS project, which can't be done
-// due to importing from `dist`. See https://stackoverflow.com/q/42609768/2083599
-export = concurrently;
 export default concurrently;
