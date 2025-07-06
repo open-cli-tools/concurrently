4fae065a1bd709d8bc3e1985a06df95cdb013a46
Gustavo Henke <guhenke@gmail.com>
2025-07-06 20:57:27 +1000

    Use local createMockInstance

--- src/flow-control/log-exit.spec.ts
+++ src/flow-control/log-exit.spec.ts
@@ -1,5 +1,4 @@
-import { createMockInstance } from 'jest-create-mock-instance';
-
+import { createMockInstance } from '../fixtures/create-mock-instance';
 import { createFakeCloseEvent, FakeCommand } from '../fixtures/fake-command';
 import { Logger } from '../logger';
 import { LogExit } from './log-exit';
