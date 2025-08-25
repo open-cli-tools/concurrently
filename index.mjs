62aa2d72a72cb121f5dbc11dc18e006404521d46
Pascal Jufer <pascal-jufer@bluewin.ch>
2025-08-25 11:45:42 +0200

    Update GH actions/workflows, enable npm provenance

--- index.mjs
+++ index.mjs
@@ -4,7 +4,6 @@
 
 import { concurrently } from './dist/src/index.js';
 
-// NOTE: the star reexport doesn't work in Node <12.20, <14.13 and <15.
 export * from './dist/src/index.js';
 
 export default concurrently;
